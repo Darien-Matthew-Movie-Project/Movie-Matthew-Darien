86ac3aec466288e6dc0c6fc9695639973bc73bbc
Matthew Moreno <matthew.r.moreno21@gmail.com>
2022-07-26 13:41:14 -0500

    Working on home page

--- jalopy-main/js/views/Home.js
+++ jalopy-main/js/views/Home.js
@@ -1,23 +1,46 @@
-import {getUser} from "../auth.js";
-
 const BASE_URI = `${BACKEND_HOST}/api/s3/download`;
 
+let info
 export default function Home(props) {
-    console.log(props);
+    info = props.movie
+    let html = topHTML();
+    html += makeMovieIntoCards();
+    html += bottomHTML();
 
+    return html;
+}
+
+function topHTML() {
     return `
-        <header>
-            <h1>Home Page</h1>
-        </header>
-        <main>
-            <div>
-                <p>
-                    This is the home page text.
-                </p>    
-            </div>
-        </main>
+    <div class="container">
     `;
 }
 
+function makeMovieIntoCards() {
+    let html = "";
+    for (let i = 0; i < info.length; i++) {
+        html += `
+        <div class="card">
+            <div class="card-header">${info[i].title}</div>
+            <div class="card-body">
+            <a id="icons"><i class="far fa-play-circle"></i></a>
+            <a id="icons"><i class="fal fa-plus-circle"></i></a>
+            <a id="icons"><i class="fal fa-heart-circle"></i></a>
+            <a id="icons"><i class="fal fa-dragon"></i></a>
+            </div>
+            <div class="card-footer"></div>
+        </div>
+        `;
+    }
+    return html;
+}
+
+function bottomHTML() {
+    return`
+    </div>
+`;
+}
+
 export function HomeEvents() {
+    console.log(info);
 }
