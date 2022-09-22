2eb0ba6c198d8867be77b6fdcfa14f8c648fc68e
Darien Salazar <darien.axell.salazar@gmail.com>
2022-09-22 13:31:22 -0500

    Completed adjustment

--- README.md
+++
@@ -1,1 +0,0 @@
-# -Movie-Project-Darien-Matthew
--- jalopy-main/js/views/Loading.js
+++
@@ -1,6 +0,0 @@
-export default function Loading(props) {
-    return `<div id="loadingView">
-                  <h1 id="loading">Loading...</h1>
-            </div>
-`;
-}
